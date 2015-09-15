db1f7ee0aa81ee3042bc57de384199857ab851a3
meist3r-ed <g.gaiax@gmail.com>
2015-09-15 13:24:07 -0300

    Tela funcionando

---
+++ assets/scripts/start.js
@@ -0,0 +1,6 @@
+function start(){
+    var overlay = document.getElementById("start");
+    var bg = document.getElementById("background");
+    overlay.style.transform = "translate(0,-100vh)";
+    bg.style.filter = "blur(0px)";
+}
